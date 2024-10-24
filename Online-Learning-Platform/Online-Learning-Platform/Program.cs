2bbce938cfe361730cd011f76d9bbf148c4fbb6e
MHW <118685507+Monwilliam@users.noreply.github.com>
2024-10-24 12:42:52 +0300

    m4 3arf

--- Online-Learning-Platform/Online-Learning-Platform/Program.cs
+++ Online-Learning-Platform/Online-Learning-Platform/Program.cs
@@ -7,54 +7,54 @@ using Online_Learning_Platform.CustomMiddleware;
 
 namespace Online_Learning_Platform
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			var builder = WebApplication.CreateBuilder(args);
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-			// Add services to the container.
-			builder.Services.AddControllersWithViews();
-
-
-			builder.Services.AddDbContext<DBContext>(options =>
-				options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
 
 
-			builder.Services.AddIdentity<AppUser, IdentityRole>()
-				.AddEntityFrameworkStores<DBContext>();
+            builder.Services.AddDbContext<DBContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+            builder.Services.AddIdentity<AppUser, IdentityRole>()
+                .AddEntityFrameworkStores<DBContext>();
 
             builder.Services.AddScoped<PermissionService>();
 
             var app = builder.Build();
 
-			// Configure the HTTP request pipeline.
-			if (!app.Environment.IsDevelopment())
-			{
-				app.UseExceptionHandler("/Home/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
-			}
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
-			app.UseHttpsRedirection();
-			app.UseStaticFiles();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
-			app.UseRouting();
+            app.UseRouting();
 
-			app.UseAuthentication();
-			app.UseAuthorization();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseMiddleware<CheckUserActiveMiddleware>();
 
             app.MapControllerRoute(
-				name: "areas",
-				pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                name: "areas",
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
-		}
-	}
+        }
+    }
 }
